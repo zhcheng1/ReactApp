d7e7978729b9b7570615f128a615badf15050b3a
Cheng <Zhuqing.Cheng@emc.com>
2017-12-01 15:34:07 -0700

    how to loop through an object with state map function

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,30 @@ var ReactDOM = require('react-dom');
 
 // create component
 var TodoComponent = React.createClass({
+    // state
+    getInitialState: function() {
+        return {
+            todos: ['React learning', 'wash'],
+            age: 30
+        }
+    },
     render: function() {
+        var todosCopy = this.state.todos;
+        todosCopy = todosCopy.map(function(item, index){ // how to loop through an object
+            return (
+                <li>{item}</li>
+            )
+        })
         return(
-            <h1>hi</h1>
+            <div id="todo-list">
+                <h1>{this.props.message}</h1>
+                <ul>{todosCopy}</ul>
+            </div>
         );
     }
 });
 
+var myList = {name:'React', status:'finished'}
+
 // put into html
-ReactDOM.render(<TodoComponent />, document.getElementById('app'));
+ReactDOM.render(<TodoComponent message="To Do List" list={myList} />, document.getElementById('app'));
